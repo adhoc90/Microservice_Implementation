ea0241828faff7f1d939290526c5ab2481b59e2b
ad_hoc <ad_hoc90@mail.ru>
2025-05-18 10:07:24 +0300

    Initial commit

---
+++ src/main/java/com/example/MicroserviceImplementation/MicroserviceImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroserviceImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MicroserviceImplementation/MicroserviceImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroserviceImplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
